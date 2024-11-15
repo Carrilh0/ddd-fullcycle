574f4ace8080f4898af9583e37c6176b3290f92c
Vitor Carrilho <vitor.carrilho@hotmail.com>
2024-11-14 22:23:35 -0300

    value objects

--- src/entity/customer.ts
+++ src/entity/customer.ts
@@ -1,7 +1,9 @@
+import { Address } from "./address";
+
 class Customer {
   private _id: string;
   private _name: string = "";
-  private _address: string = "";
+  private _address!: Address;
   private _active: boolean = true;
 
   constructor(id: string, name: string) {
@@ -25,7 +27,7 @@ class Customer {
   }
 
   activate() {
-    if(this._address.length === 0) {
+    if(this._address !== undefined) {
       throw new Error("Address is mandatory to activate a customer")
     }
     this._active = true
@@ -34,4 +36,12 @@ class Customer {
   deactivate() {
     this._active = false
   }
+
+  set Address(address: Address) {
+    this._address = address
+  }
 }
+
+const customer = new Customer('1','Vitor')
+
+console.log(customer.Address = new Address('asd', 2,'2324', 'SSA'))
