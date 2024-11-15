1438fc6ce5911e20c2942a55762069f252dbe4c4
Vitor Carrilho <vitor.carrilho@hotmail.com>
2024-11-15 17:44:20 -0300

    product class

--- src/entity/order.spec.ts
+++ src/entity/order.spec.ts
@@ -21,12 +21,19 @@ describe("Order unit test", () => {
   })
 
   it("should calculate total", () => {
-    const item1 = new OrderItem("1", "Item 1", 100)
-    const item2 = new OrderItem("2", "Item 2", 400)
-    const item3 = new OrderItem("3", "Item 3", 200)
+    const item1 = new OrderItem("1", "Item 1", 100, "p1", 2)
+    const item2 = new OrderItem("2", "Item 2", 400, "p2", 3)
+    const item3 = new OrderItem("3", "Item 3", 200, "p3", 1)
     let order = new Order("123","123",[item1,item2,item3])
     const total = order.total()
-    
-    expect(total).toBe(700)
+
+    expect(total).toBe(1600)
   })
+
+  it("should throw error if the qte is less or equal 0", () => {
+    expect(() => {
+      const item = new OrderItem("i1", "Item", 100, "p1", 0);
+      const order = new Order("01", "c1", [item]);
+      }).toThrow("Quantity must be greater than 0");
+  });
 });
