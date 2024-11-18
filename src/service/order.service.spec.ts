4cd840900de1a0dfcb971df1fa0d5e06ac199f67
Vitor Carrilho <vitor.carrilho@hotmail.com>
2024-11-18 13:13:26 -0300

    added reward points

--- src/service/order.service.spec.ts
+++ src/service/order.service.spec.ts
@@ -1,8 +1,18 @@
+import { Customer } from "../entity/customer"
 import { Order } from "../entity/order"
 import { OrderItem } from "../entity/orderItem"
 import { OrderService } from "./order.service"
 
 describe("Order service unit tests", () => {
+
+  it("should place an order", () => {
+    const customer = new Customer("1","Customer 1")
+    const item1 = new OrderItem("1", "Item 1", 100, "p1", 2)
+    const order = OrderService.placeOrder(customer, [item1])
+
+    expect(customer.rewardPoints).toBe(100)
+    expect(order.total()).toBe(200)
+  }) 
   it("should calculate total", () => {
     const item1 = new OrderItem("1", "Item 1", 100, "p1", 2)
     const item2 = new OrderItem("2", "Item 2", 400, "p2", 2)
